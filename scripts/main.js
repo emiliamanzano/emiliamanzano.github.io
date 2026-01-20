2c0bacd191a128f3456126278e7f8ef989a6a4a4
Emília Manzano <felipemanzano@usp.br>
2026-01-20 17:18:33 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,8 @@ const myImage = document.querySelector("img");
 myImage.addEventListener("click", () => {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "images/imagem_emilia.png") {
-    myImage.setAttribute("src" = "images/mario_luigi.png");
+    myImage.setAttribute("src", "images/mario_luigi.png");
   } else {
-    myImage.setAttribute("src" = "images/imagem_emilia.png");
+    myImage.setAttribute("src", "images/imagem_emilia.png");
   }
 });
