9840ed7b80b4d7d1dc86c10a711ecb6735c8670d
Emília Manzano <felipemanzano@usp.br>
2026-01-20 16:59:18 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
-const myHeading=document.querySelector("h1");
-myHeading.textContent = "Hello World!";
+const myImage = document.querySelector("img")
+//ISSO É UM COMENTÁRIO JAVA//
+myImage.addEventListener("click", () => {
+  const mySrc = myImg.getAttribute("src");
+  if (mySrc === "images/imagem_emilia.png") {
+    myImage.setAttribute("src" = "images/mario_luigi.png");
+  } else {
+    myImage.setAttribute("src" = "images/imagem_emilia.png");
+  }
+});
